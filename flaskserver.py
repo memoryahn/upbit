cb328568d84e2ae78154ae0a4c9895e201c207cc
labtopSSAhn <memoryahn@gmail.com>
2018-02-19 21:36:20 +0900

    상승률 순위까지

--- flaskserver.py
+++ flaskserver.py
@@ -1,21 +1,177 @@
 from flask import Flask, jsonify, request
 from flask_pymongo import PyMongo
+import urllib.request
+import json
+from flask_cors import CORS
+from datetime import datetime
+
+
+
 
 app = Flask(__name__)
+CORS(app)
 
 app.config['MONGO_DBNAME'] = 'upbit'
 app.config['MONGO_HOST'] = 'mongodb://220.230.124.148:27017'
 app.config['MONGO_PORT'] = '27017'
 
 mongo = PyMongo(app)
+coin = [
+{"name" :"비트코인"
+,"code":"BTC"},
 
-@app.route('/api/data',methods=['POST'])
+{"name" :"에이다"
+,"code":"ADA"},
+
+{"name" :"리플"
+,"code":"XRP"},
+
+{"name" :"비트코인캐시"
+,"code":"BCC"},
+
+{"name" :"이더리움"
+,"code":"ETH"},
+
+{"name" :"퀀텀"
+,"code":"QTUM"},
+
+{"name" :"네오"
+,"code":"NEO"},
+
+{"name" :"뉴이코노미무브먼트"
+,"code":"XEM"},
+
+{"name" :"스테이터스네트워크토큰"
+,"code":"SNT"},
+
+{"name" :"리스크"
+,"code":"LSK"},
+
+{"name" :"스텔라루멘"
+,"code":"XLM"},
+
+{"name" :"라이트코인"
+,"code":"LTC"},
+
+{"name" :"이더리움클래식"
+,"code":"ETC"},
+
+{"name" :"스팀달러"
+,"code":"SBD"},
+
+{"name" :"스팀"
+,"code":"STEEM"},
+
+{"name" :"비트코인골드"
+,"code":"BTG"},
+
+{"name" :"어거"
+,"code":"REP"},
+
+{"name" :"아더"
+,"code":"ARDR"},
+
+{"name" :"메탈"
+,"code":"MTL"},
+
+{"name" :"웨이브"
+,"code":"WAVES"},
+
+{"name" :"머큐리"
+,"code":"MER"},
+
+{"name" :"오미세고"
+,"code":"OMG"},
+
+{"name" :"스트라티스"
+,"code":"STRAT"},
+
+{"name":"파워렛저"
+,"code":"POWR"},
+
+{"name" :"블록틱스"
+,"code":"TIX"},
+
+{"name" :"아인스타이늄"
+,"code":"EMC2"},
+
+{"name" :"피벡스"
+,"code":"PIVX"},
+
+{"name" :"스토리지"
+,"code":"STORJ"},
+
+{"name" :"코모도"
+,"code":"KMD"},
+
+{"name" :"그로스톨코인"
+,"code":"GRS"},
+
+{"name" :"모네로"
+,"code":"XMR"},
+
+{"name" :"아크"
+,"code":"ARK"},
+
+{"name" :"버트코인"
+,"code":"VTC"},
+
+{"name" :"지캐시"
+,"code":"ZEC"},
+
+{"name" :"대시"
+,"code":"DASH"},
+]
+
+
+@app.route('/',methods=['GET'])
 def get_data():
-    # urlltc='https://crix-api-endpoint.upbit.com/v1/crix/candles/minutes/1?code=CRIX.UPBIT.KRW-LTC&count=1'
-    # headersObject = { 'User-Agent': '', 'Accept': '*/*' }
-    # with urllib.request.urlopen(urllib.request.Request(urlbtc, headers = headersObject)) as btcUrl:
-    #     btcData = btcUrl.read()
-    #     encoding = btcUrl.info().get_content_charset('utf-8')    
-    #     upbitBtc=json.loads(btcData.decode(encoding))[0]['tradePrice']
-    return 'data'
+    data=[]
+    count=0
+    for code in coin:
+        url='https://crix-api-endpoint.upbit.com/v1/crix/candles/minutes/1?code=CRIX.UPBIT.KRW-'+code['code']+'&count=1'
+        headersObject = { 'User-Agent': '', 'Accept': '*/*' }
+        with urllib.request.urlopen(urllib.request.Request(url, headers = headersObject)) as coinUrl:
+            coinData = coinUrl.read()
+            encoding = coinUrl.info().get_content_charset('utf-8')    
+            tradePrice=json.loads(coinData.decode(encoding))[0]['tradePrice']
+            candleDateTimeKst=json.loads(coinData.decode(encoding))[0]['candleDateTimeKst']
+            openingPrice=json.loads(coinData.decode(encoding))[0]['openingPrice']
+            highPrice=json.loads(coinData.decode(encoding))[0]['highPrice']
+            lowPrice=json.loads(coinData.decode(encoding))[0]['lowPrice']
+        result={'name':code['name'],'code':code['code'],
+        "tradePrice":tradePrice,
+        "candleDateTimeKst": candleDateTimeKst,
+        "openingPrice": openingPrice,
+        "highPrice": highPrice, 
+        "lowPrice": lowPrice,        
+        }
+        data.append(result)
+        count=count+1
+    tempdate = data[0]['candleDateTimeKst'].replace("+09:00","")
+    d =datetime.strptime(tempdate ,'%Y-%m-%dT%H:%M:%S')
+    print(d.hour)
+    return jsonify(data)
+@app.route('/api/open',methods=['GET'])
+def get_open():
+    data=[]
+    for code in coin:
+        url='https://crix-api-endpoint.upbit.com/v1/crix/candles/minutes/60?code=CRIX.UPBIT.KRW-'+code['code']+'&count=24'
+        headersObject = { 'User-Agent': '', 'Accept': '*/*' }
+        with urllib.request.urlopen(urllib.request.Request(url, headers = headersObject)) as coinUrl:
+            coinData = coinUrl.read()
+            encoding = coinUrl.info().get_content_charset('utf-8')    
+            for i in json.loads(coinData.decode(encoding)):
+                candleDateTimeKst = i['candleDateTimeKst']
+                tempdate = candleDateTimeKst.replace("+09:00","")
+                d =datetime.strptime(tempdate ,'%Y-%m-%dT%H:%M:%S')
+                if d.hour == 7:
+                    openingPrice=i['openingPrice']
+        result={'name':code['name'],'code':code['code'],'open':openingPrice}
+        data.append(result)
+    return jsonify(data)
+
+
+if __name__=='__main__':
+    app.run(debug=True)
 
--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,56 +1,131 @@
 <template>
-<div>
-  <h1>upbit</h1>
-  <button class="btn" @click="stop">stop</button>
+<div style="padding:20px">
+<div align="center" class="row" style="margin-top:10px">
+  <div>
+    <div align="left">총코인 : 35</div>
+    <div>+5% 코인 : {{ count }}</div>
+  </div>
+<div class="col-12">
+    <div class="row" style="font-size:10px">
+      <div class="title col-2">이름</div>
+      <div class="title col-1">코드</div>
+      <div class="title col-1">OPEN 가격</div>
+      <div class="title col-1">현재 가격</div>
+      <div class="title col-1">상승률</div>
+      <div class="title col-1">최고</div>
+      <div class="title col-1">최저</div>
+      <div class="title col-1">순위</div>
+      <div class="title col-3">그래프</div>
+    </div>
+  <div class="row" style="font-size:10px" v-for="(d,i) in data" :key="i">
+    <div align="left" class="col-2" style=";border:1px solid lightgray">
+    {{ d.name }}
+    </div>
+    <div class="col-1" style=";border:1px solid lightgray">
+    {{ d.code }}
+    </div>
+    <div align="right" class="col-1" style=";border:1px solid lightgray">
+    {{ comma(d.open) }}
+    </div>
+    <div align="right" class="col-1" style=";border:1px solid lightgray">
+    {{ comma(d.tradePrice) }}
+    </div>
+    <div align="left" v-if="d.climeRate >= 5" class="col-1" 
+      style="color:red;border:1px solid lightgray">▲ {{ d.climeRate }}%</div>
+    <div align="left" v-if="d.climeRate < 5" class="col-1" 
+      style="color:blue;border:1px solid lightgray">▼ {{ d.climeRate }}%</div>
+    <div class="col-1" style="color:red;border:1px solid lightgray">▲ +15%</div>
+    <div class="col-1" style="color:blue;border:1px solid lightgray">▼ -10%</div>
+    <div class="col-1" style="color:red;border:1px solid lightgray">( {{ d.rate }} )</div>
+    <div class="col-3" style=";border:1px solid lightgray">[     |      ]</div>
+  </div>
+</div>
+          </div> 
 </div>
 </template>
 <script>
 import axios from 'axios'
 import Vue from 'vue'
-var sockjs = new SockJS("https://crix-websocket.upbit.com/sockjs")
+
+
 export default {
   data() {
     return {
-      data:[],
-      inter:null,
-      count:1,
+      data:{},
+      count:0
     }
   },
   methods: {
+    comma(nStr){
+      nStr += '';
+      var x = nStr.split('.');
+      var x1 = x[0];
+      var x2 = x.length > 1 ? '.' + x[1] : '';
+      var rgx = /(\d+)(\d{3})/;
+      while (rgx.test(x1)) {
+        x1 = x1.replace(rgx, '$1' + ',' + '$2');
+      }
+      return x1 + x2;
+    },
     stop() {
-            sockjs.close()
-            console.log("Socket closed")
     },
     getdata() {
-      sockjs.onopen = function () {
-        console.log("Socket open");
-        var s = [
-          {
-            ticket: "ram macbook"
-          },
-          {
-            type: "recentCrix",
-            codes: ["CRIX.UPBIT.KRW-BTC"]
-            //codes: ["CRIX.UPBIT.KRW-BTC", "CRIX.BITFINEX.USD-BTC", "CRIX.BITFLYER.JPY-BTC", "CRIX.OKCOIN.CNY-BTC", "CRIX.KRAKEN.EUR-BTC", "CRIX.UPBIT.KRW-DASH", "CRIX.UPBIT.KRW-ETH", "CRIX.UPBIT.KRW-NEO", "CRIX.UPBIT.KRW-BCC", "CRIX.UPBIT.KRW-MTL", "CRIX.UPBIT.KRW-LTC", "CRIX.UPBIT.KRW-STRAT", "CRIX.UPBIT.KRW-XRP", "CRIX.UPBIT.KRW-ETC", "CRIX.UPBIT.KRW-OMG", "CRIX.UPBIT.KRW-SNT", "CRIX.UPBIT.KRW-WAVES", "CRIX.UPBIT.KRW-PIVX", "CRIX.UPBIT.KRW-XEM", "CRIX.UPBIT.KRW-ZEC", "CRIX.UPBIT.KRW-XMR", "CRIX.UPBIT.KRW-QTUM", "CRIX.UPBIT.KRW-LSK", "CRIX.UPBIT.KRW-STEEM", "CRIX.UPBIT.KRW-XLM", "CRIX.UPBIT.KRW-ARDR", "CRIX.UPBIT.KRW-KMD", "CRIX.UPBIT.KRW-ARK", "CRIX.UPBIT.KRW-STORJ", "CRIX.UPBIT.KRW-GRS", "CRIX.UPBIT.KRW-VTC", "CRIX.UPBIT.KRW-REP", "CRIX.UPBIT.KRW-EMC2", "CRIX.UPBIT.KRW-ADA", "CRIX.UPBIT.KRW-SBD", "CRIX.UPBIT.KRW-TIX", "CRIX.UPBIT.KRW-POWR", "CRIX.UPBIT.KRW-MER", "CRIX.UPBIT.KRW-BTG", "CRIX.COINMARKETCAP.KRW-USDT"]
-          },
-          {
-            type: "crixTrade",
-            codes: ["CRIX.UPBIT.KRW-BTC"]
-          },
-          {
-            type: "crixOrderbook",
-            codes: ["CRIX.UPBIT.KRW-BTC"]
-          }
-        ];
-        sockjs.send(JSON.stringify(s));
-        sockjs.onmessage = function (e) {
-          console.log(JSON.parse(e.data));
-        };
-      };
+       axios.get('http://127.0.0.1:5000')
+    .then(response=>{
+      console.log('connect')
+      this.data1=response.data
+
+    })
+    .catch(e=>{
+      console.log(e)
+    })
       
     }
   },
   created() {
-   this.getdata() 
+    axios.get('http://127.0.0.1:5000/api/open')
+    .then(response=>{
+      console.log('connect')
+      this.data=response.data
+      axios.get('http://127.0.0.1:5000')
+    .then(response=>{
+      console.log('connect2')
+      this.count=0
+      for(let i in this.data){
+        for(let k in response.data){
+        if(this.data[i].code==response.data[k].code){
+          this.data[i].tradePrice=response.data[k].tradePrice
+          Vue.set(this.data[i],'climeRate',(((this.data[i].tradePrice-this.data[i].open)/this.data[i].open)*100).toFixed(2))
+          if(this.data[i].climeRate>=5){
+            this.count++
+          }
+        }
+        }
+      }
+      this.data.sort(function(b, a){return a.climeRate - b.climeRate});
+      for(let i in this.data){
+      Vue.set(this.data[i],'rate',Number(i)+1)
+      }
+    })
+    .catch(e=>{
+      console.log(e)
+    })
+      // for(let i in this.data){
+      //   this.data[i].open=addCommas(this.data[i].open)
+      // }
+    })
+    .catch(e=>{
+      console.log(e)
+    })
+
+    
+    
   }
-}</script>
+}
+</script>
+<style>
+.title {
+  border:1px solid lightgray;
+  color:brown;
+}
+</style>
